5b2ca0cad4063fde7ef5d7c4080ae0d5d0f36973
birukshegute <birukshegute@gmail.com>
2023-12-13 23:06:19 -0800

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Grants all permissions for user_0d_1
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT SELECT ON *.* TO 'user_0d_1'@'localhost';
+GRANT *.* TO 'user_0d_1'@'localhost';
