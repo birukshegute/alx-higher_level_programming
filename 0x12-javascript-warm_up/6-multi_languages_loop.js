3a7c887bce07edc938fae2e0b1a9386b35bb2bc0
birukshegute <birukshegute@gmail.com>
2024-01-08 19:03:29 -0800

    task 6 redone

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // prints 3 lines using an array and a loop
 
-const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 for (let i = 0; i < 3; i++) {
-  console.log(langs[i]);
+  console.log(lines[i]);
 }
