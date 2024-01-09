02c77b61814820fc875361236afdddf1aafbbdb7
birukshegute <birukshegute@gmail.com>
2024-01-08 18:36:52 -0800

    task 1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,10 +1,8 @@
-#!sr/bin/node
+#!/usr/bin/node
 // Prints 3 lines
 
 var1 = 'C is fun';
 var2 = 'Python is cool';
 var3 = 'JavaScript is amazing';
 
-console.log(var1 /n);
-console.log(var2 /n);
-console.log(var3);
+console.log(var1 + '\n' + var2 + '\n' + var3);
