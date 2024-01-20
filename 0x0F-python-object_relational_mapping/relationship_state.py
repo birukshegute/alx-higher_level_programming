8760b6dcb4534d2e77b77aafc2d93906acb52cb3
birukshegute <birukshegute@gmail.com>
2024-01-19 16:41:34 -0800

    bonus task

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -9,6 +9,7 @@ from relationship_city import Base, City
 
 
 class State(Base):
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
 
