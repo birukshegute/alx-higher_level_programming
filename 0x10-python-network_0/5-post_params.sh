4e75f462df164745a189098d21db1acc30b0b9c1
birukshegute <birukshegute@gmail.com>
2024-01-25 13:26:28 -0800

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
-##!/bin/bash
+#!/bin/bash
 # A script that takes in a URL, sends a POST request, and displays the body.
 curl -s -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -X POST "$1"
