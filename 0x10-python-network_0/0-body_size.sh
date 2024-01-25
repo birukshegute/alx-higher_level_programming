b7cdcc3b18c0f832f92e40f39067c43420e18a21
birukshegute <birukshegute@gmail.com>
2024-01-25 12:54:59 -0800

    task 0, 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# A Bash script that takes in a URL, sends a request to that URL, 
-# and displays the size of the body
+# A script that takes in a URL, sends a request to that URL, and displays the size
 curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
