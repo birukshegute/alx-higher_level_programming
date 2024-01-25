a54bcb6aff855a8e7d9ae31c3c85b422717a280e
birukshegute <birukshegute@gmail.com>
2024-01-25 13:08:41 -0800

    task 0, 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in a URL, sends a request to that URL, and displays the size
-curl -sI ALLOW $1 -L | grep "Allow" | cut -d " " -f2-
+curl -Is "$1" | grep -w 'Content-Length' | cut -f2 -d' '
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a DELETE request to the URL passed as argument and displays the body
-curl -sX GET $1 -L
+curl -sX GET "$1" -L
