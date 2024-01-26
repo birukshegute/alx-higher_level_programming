06b394b85b1642a6061b3fad3f4fec9e7cc312fa
birukshegute <birukshegute@gmail.com>
2024-01-26 15:23:34 -0800

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     myVar = {"q": argv[1] if len(argv) > 1 else ""}
     req = requests.post("http://0.0.0.0:5000/search_user", data=myVar)
     try:
-        json = request.json()
+        json = req.json()
         if json:
             print("[{}] {}".format(json.get("id"), json.get("name")))
         else:
